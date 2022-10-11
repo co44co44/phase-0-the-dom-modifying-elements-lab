133be581a9bd0e81c942de1abde2dc16f4bf3c30
Maria Cristina Ordonez <crisordonez44@gmail.com>
2022-10-11 15:11:57 -0500

    commit tree 5864958e10389c91f0896b81db716c646f230ead

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+// Write your code here!cd .
+const newHeader= document.createElement ("h1");
+newHeader.setAttribute ("id", "h1");
+newHeader.id="victory";
+newHeader.textContent = 'CRISTINA is the champion';
